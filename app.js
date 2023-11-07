57845227079b58e1cb8c34f7d0d0edc3dc6ca91c
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2023-11-07 14:23:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import taskRouter from './routes/task.js';
 import { config } from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { errorMiddleware } from './middlewares/error.js';
-import cors from "cors";
+const cors = require ('cors')
 
 export const app = express();
 config({
@@ -13,13 +13,10 @@ config({
 
 
 //Using Middleware
-app.use(express.json())         
+app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT","DELETE"],
-    credentials: true,
-}))
+
 
 // Using Routes
 app.use("/api/v1/users",userRouter);

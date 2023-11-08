ccb3e19b0e8e7a756ba735931e351b8a8198e4ae
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2023-11-08 11:16:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ config({
     path: "./data/config.env"
 })
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //Using Middleware
 app.use(cors());
